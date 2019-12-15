34eea6d1bbd6d34f462f178290775d4d03200035
Kei Kamikawa <code-hex@users.noreply.github.com>
2019-12-15 18:49:54 +0900

    Added code for ansi color

---
+++ ansi.go
@@ -0,0 +1,50 @@
+package wordwrap
+
+import (
+	"strings"
+	"unicode"
+)
+
+const esc = "\x1b["
+
+func trimAnsiColor(s string) (ret string) {
+	for ret = s; ; {
+		startPos, endPos := rangeAnsiColor(ret)
+		if startPos < 0 {
+			return ret
+		}
+		if startPos == 0 {
+			ret = ret[endPos:]
+		} else {
+			tmp := ret[:startPos]
+			ret = tmp + ret[endPos:]
+		}
+	}
+}
+
+func rangeAnsiColor(s string) (int, int) {
+	escIndex := strings.Index(s, esc)
+	if escIndex < 0 {
+		return -1, -1
+	}
+	tmp := s[escIndex+len(esc):]
+	// Invalid ansi format: "\x1b[<ascii character>"
+	if len(tmp) <= 1 {
+		return -1, -1
+	}
+	for i, c := range tmp {
+		if c == 'm' {
+			// returns start position and the length
+			//
+			// i.e. "hello\x1b[38;5;82m", length == 15
+			// return 5, (5 + len(ansi) == 15)
+			return escIndex, escIndex + (len(esc) + i + 1)
+		}
+		if unicode.IsDigit(c) || c == ';' {
+			continue
+		}
+	}
+
+	// Nothing 'm' period
+	return -1, -1
+}
